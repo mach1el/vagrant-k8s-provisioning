6ebd998b6ffd6e755918aa9f950742c02a40b354
mach1el <th3phantom.m1@gmail.com>
2022-09-24 15:14:20 +0700

    Use calico network

--- master_bootstrap.sh
+++ master_bootstrap.sh
@@ -12,7 +12,7 @@ cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 
 echo "[TASK 3] Deploy Calico network"
-kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.24.1/manifests/custom-resources.yaml
 kubectl taint nodes --all node-role.kubernetes.io/master- >/dev/null 2>&1
 
 echo "[TASK 4] Generate and save cluster join command to /joincluster.sh"
