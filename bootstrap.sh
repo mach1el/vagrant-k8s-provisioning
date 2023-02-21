d1cc495dff8b38c213dc817ad5c6a12c46a924d3
mach1el <th3phantom.m1@gmail.com>
2023-02-21 17:22:03 +0700

    Using conainterd.io and flannel network

--- bootstrap.sh
+++ bootstrap.sh
@@ -28,7 +28,10 @@ EOF
 sysctl --system >/dev/null 2>&1
 
 echo "[TASK 5] Install and Configure containerd"
-apt update >/dev/null 2>&1 && apt install -yqq containerd >/dev/null 2>&1
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add - >/dev/null 2>&1
+echo "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list >/dev/null 2>&1
+apt update >/dev/null 2>&1 && apt install -yqq containerd.io >/dev/null 2>&1
+rm /etc/containerd/config.toml >/dev/null 2>&1
 wget -O /etc/containerd/config.toml https://gist.githubusercontent.com/mach1el/35d457d624395c85ac63ce73b7337f86/raw/a753c3c34d56f2c30150aa38a29028e84a889061/config.toml >/dev/null 2>&1
 systemctl restart containerd >/dev/null 2>&1
 systemctl enable containerd >/dev/null 2>&1
@@ -39,17 +42,27 @@ apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main" >/dev/
 apt update -qq >/dev/null 2>&1
 apt install -qq -y kubelet kubeadm kubectl >/dev/null 2>&1
 
-echo "[TASK 7] Enable ssh password authentication"
+echo "[TASK7] Flannel configuration"
+rm -f /etc/cni/net.d/*flannel* >/dev/null 2>&1
+mkdir -p /run/flannel >/dev/null 2>&1
+cat >>/run/flannel/subnet.env<<EOF
+FLANNEL_NETWORK=10.240.0.0/16
+FLANNEL_SUBNET=10.240.0.1/24
+FLANNEL_MTU=1450
+FLANNEL_IPMASQ=true
+EOF
+
+echo "[TASK 8] Enable ssh password authentication"
 sed -i 's/^PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
 systemctl reload sshd
 
-echo "[TASK 8] Update /etc/resolv.conf"
+echo "[TASK 9] Update /etc/resolv.conf"
 cat >/etc/resolv.conf<<EOF
 nameserver 8.8.8.8
 nameserver 8.8.4.4
 EOF
 
-echo "[TASK 9] Update /etc/hosts file"
+echo "[TASK 10] Update /etc/hosts file"
 cat >>/etc/hosts<<EOF
 10.98.1.10   kmaster.demo     kmaster
 10.98.1.11   kworker1.demo    kworker1
--- master_bootstrap.sh
+++ master_bootstrap.sh
@@ -4,7 +4,7 @@ echo "[TASK 1] Pull required containers"
 kubeadm config images pull >/dev/null 2>&1
 
 echo "[TASK 2] Initialize Kubernetes Cluster"
-kubeadm init --apiserver-advertise-address=10.98.1.10 --pod-network-cidr 192.168.0.0/16 >> /root/kubeinit.log 2>/dev/null
+kubeadm init --apiserver-advertise-address=10.98.1.10 --pod-network-cidr=10.244.0.0/16 >> /root/kubeinit.log 2>/dev/null
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
@@ -14,7 +14,8 @@ systemctl restart containerd
 systemctl restart kubelet
 
 echo "[TASK 4] Deploy Calico network"
-kubectl apply -f "https://docs.projectcalico.org/manifests/calico.yaml" >/dev/null 2>&1
+# kubectl apply -f "https://docs.projectcalico.org/manifests/calico.yaml" >/dev/null 2>&1
+kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml >/dev/null 2>&1
 kubectl taint nodes --all node-role.kubernetes.io/control-plane- >/dev/null 2>&1
 
 echo "[TASK 5] Generate and save cluster join command to /joincluster.sh"
